14af5f912f20da5d22a55ed465f7512b0286c62d
Raj31015 <154362399+Raj31015@users.noreply.github.com>
2025-04-12 00:09:07 +0530

    Update data-charts.tsx

--- components/data-charts.tsx
+++ components/data-charts.tsx
@@ -4,6 +4,7 @@ import {Chart} from "./chart"
 import { SpendingPie } from "./spending-pie";
 export const DataCharts=()=>{
     const {data}=useGetSummary();
+    console.log(data);
     return(
         <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
             <div className="col-span-1 lg:col-span-3 xl:col-span-4">
