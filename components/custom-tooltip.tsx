64e5ff584860ba48572ac1d63c19eb0a9c56ad0c
Raj31015 <154362399+Raj31015@users.noreply.github.com>
2025-04-12 10:31:47 +0530

    Update custom-tooltip.tsx

--- components/custom-tooltip.tsx
+++ components/custom-tooltip.tsx
@@ -3,10 +3,11 @@ import { formatCurrency } from "@/lib/utils"
 import {Separator} from "@/components/ui/separator"
 
 export const CustomTooltip=({active,payload}:any)=>{
-    if(!active) return null;
-    const date=payload[0].payload.date;
-    const income=payload[0].value;
-    const expenses=payload[1].value
+if (!active || !payload || !payload[0] || !payload[1]) return null;
+
+  const date = payload[0].payload?.date;
+  const income = payload[0].value;
+  const expenses = payload[1].value;
     return(
         <div className="rounded-sm bg-white shadow-sm border overflow-hidden">
             <div className="text-sm p-2 px-3 bg-muted text-muted-foreground">
