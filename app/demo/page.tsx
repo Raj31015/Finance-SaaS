36d2cc2cfdf0fc5c70ce9861e9b3671c836bf3bf
Raj31015 <154362399+Raj31015@users.noreply.github.com>
2025-12-08 19:03:46 +0530

    Update page.tsx

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,12 +1,12 @@
 import { redirect } from "next/navigation";
-import type { NextApiRequest, NextApiResponse } from "next";
+
 export default function DemoPage(req: NextApiRequest, res: NextApiResponse) {
   const demoEmail = "demouser@example.com";
 
   const signInUrl = new URL("https://relaxing-fish-40.accounts.dev/sign-in");
 
   // Clerk prefill
-  signInUrl.searchParams.set("prefill_email", demoEmail);
+  signInUrl.searchParams.set("email_address", demoEmail);
 
   // Optional: after login redirect back to dashboard
   signInUrl.searchParams.set("redirect_url", "/");
